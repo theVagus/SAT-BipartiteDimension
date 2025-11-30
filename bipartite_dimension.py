c1f1a808deef1243035bd91661f62105323e5f10
Vilda <vilikjanda@proton.me>
2025-11-30 21:42:00 +0100

    Added Bvertice constraints!!!! indexing fixes and got rid of debugging help and overall clean up

--- bipartite_dimension.py
+++ bipartite_dimension.py
@@ -1,29 +1,38 @@
 import subprocess
 import numpy as np
+import time
 from argparse import ArgumentParser
 
+import re
 
 class Graph:
     def __init__(self, vertices, edges, edgeliterals):
-        self.vertices = vertices
+        self.vertices = list(vertices)   # expect range(...) or list
+        self.n = len(self.vertices)
         self.edges = edges
-        self.edgeliterals = edgeliterals # mapping of edge id to list
-        #self.verticesliterals = len(vertices)
-        self.numberofedgeliterals = len(vertices)*(len(vertices)-1)//2 # to avoid unnecessary literals we have complete graph number of edges
-        #self.edgeliterals = len(vertices)*len(vertices)
+        self.edgeliterals = edgeliterals
+        # number edges in complete graph on n vertices
+        self.numberofedgeliterals = self.n * (self.n - 1) // 2
 
-
-    def get_edge_id(self, u, v, lits_before=0):
+    def get_edge_id(self, u, v): # triangular number mapping of edge (u,v) to id in [0, n(n-1)/2-1]
         if u > v:
             u, v = v, u
-        return (u - 1) * (len(self.vertices) - u // 2) + (v - u) - 1 + lits_before
+        n = self.n
+        return (u - 1) * (2 * n - (u)) // 2 + (v - u - 1)
+
     def get_biclique_edge_id(self, b, u, v, lits_before=0):
-        edge_id = self.get_edge_id(u, v)
-        return (b-1) * self.numberofedgeliterals + edge_id + 1 + lits_before
-    def get_Abiclique_vertice_id(self, b, v, lits_before=0):
-        return (b-1) * len(self.vertices) + v + lits_before +1
-    def get_Bbiclique_vertice_id(self, b, v, lits_before=0): #same as A but is used for clarity
-        return (b-1) * len(self.vertices) + v  + lits_before +1
+
+        edge_id = self.get_edge_id(u, v)  # 0-lits_befored within one biclique
+        var_id = lits_before + (b - 1) * self.numberofedgeliterals + edge_id + 1
+        return var_id
+
+    def get_A_vertice_id(self, b, v, lits_before=0):        # layout: after all biclique-edge vars we place all A-variables blocks
+        var_id = lits_before + (b - 1) * self.n + (v - 1) + 1
+        return var_id
+
+    def get_B_vertice_id(self, b, v, lits_before=0):
+        var_id = lits_before + (b - 1) * self.n + (v - 1) + 1
+        return var_id
 
     
 
@@ -51,101 +60,80 @@ def edges_to_literals(edges): # convert adjacency matrix to edge literals list
     return edgelit
 
 
-def encode(graph,k):
-    
-
-    cnf = [] # list of clauses
+def encode(graph, k):
+    cnf = []
+    # setup language in this order:
+    # 1) biclique-edge variables: k blocks
+    # 2) A variables: k blocks, each of size graph.n
+    # 3) B variables: k blocks, each of size graph.n
 
     nr_vars = 0
-    literalsbeforeedges= nr_vars
+    lits_before_edges = nr_vars
+    nr_vars += k * graph.numberofedgeliterals
 
+    lits_before_A = nr_vars
+    nr_vars += k * graph.n
 
+    lits_before_B = nr_vars
+    nr_vars += k * graph.n
 
-    # Add literals for edges present in k-biclique
-    biclique_edges = []
-    for i in range(k):
-        biclique_edges.append(graph.edgeliterals)
-    # if edge exists in graph it must exist in at least one biclique
-    for i in range(len(graph.edgeliterals)):
-        is_edge, u, v = graph.edgeliterals[i]
+    # If an edge exists in the graph it must appear in at least one biclique:
+    for (is_edge, u, v) in graph.edgeliterals:
         if is_edge:
             clause = []
-            for b in range(1,k+1):
-                biclique_edge_lit = graph.get_biclique_edge_id(b, u, v,nr_vars)
-                clause.append(biclique_edge_lit)
+            for b in range(1, k+1):
+                clause.append(graph.get_biclique_edge_var(b, u, v, lits_before=lits_before_edges))
             clause.append(0)
             cnf.append(clause)
 
-
-
-    # if edge does not exist in graph it must not exist in any biclique?
-    for i in range(len(graph.edgeliterals)):
-        is_edge, u, v = graph.edgeliterals[i]
+    # If an edge does not exist then it must not be chosen in any biclique:
+    for (is_edge, u, v) in graph.edgeliterals:
         if not is_edge:
-            for b in range(1,k+1):
-                biclique_edge_lit = graph.get_biclique_edge_id(b, u, v)
-                cnf.append([-biclique_edge_lit, 0])
-    nr_vars += k * graph.numberofedgeliterals
-    literalsbefore_A= nr_vars
+            for b in range(1, k+1):
+                evar = graph.get_biclique_edge_var(b, u, v, lits_before=lits_before_edges)
+                cnf.append([-evar, 0])
 
+    # If an edge is chosen in a biclique then its vertice must be exclusively in A or B part:
+    for (is_edge, u, v) in graph.edgeliterals:
+        for b in range(1, k+1):
+            evar = graph.get_biclique_edge_var(b, u, v, lits_before=lits_before_edges)
+            A_u = graph.get_A_var(b, u, lits_before=lits_before_A)
+            A_v = graph.get_A_var(b, v, lits_before=lits_before_A)
+            B_u = graph.get_B_var(b, u, lits_before=lits_before_B)
+            B_v = graph.get_B_var(b, v, lits_before=lits_before_B)
 
+            # converted to CNF 
+            cnf.append([-evar, A_u, A_v, 0])
+            cnf.append([-evar, A_u, B_u, 0])
+            cnf.append([-evar, B_v, A_v, 0])
+            cnf.append([-evar, B_v, B_u, 0])
+            cnf.append([-evar, -A_u, -B_u, -A_v, -B_v, 0])
 
-
-    # setup a vertices in biclique A and B
-    vertices_in_biclique_A = []
-    vertices_in_biclique_B = []
-    for i in range(k):
-        A = []
-        for v in graph.vertices:
-            A.append((graph.get_Abiclique_vertice_id(i+1, v,nr_vars),v+1))
-        vertices_in_biclique_A.append(A)
-    nr_vars +=  k * len(graph.vertices)
-    literalsbefore_B= nr_vars
-    for i in range(k):
-        B = []
-        for v in graph.vertices:
-            B.append((graph.get_Bbiclique_vertice_id(i+1, v,nr_vars),v+1)) 
-        vertices_in_biclique_B.append(B)     
-    nr_vars +=  k * len(graph.vertices)
-
-
-
-    # if an edge is present in a biclique k, then u in A_k and v in B_k or v in A_k and u in B_k , NOT both
-    literalsbefore_A -= 1
-    literalsbefore_B -= 1 
-    for i in range(len(graph.edgeliterals)):
-        _, u, v = graph.edgeliterals[i]
-        for b in range(1,k+1):
-            biclique_edge_lit = graph.get_biclique_edge_id(b, u, v,literalsbeforeedges)
-            A_u_lit = graph.get_Abiclique_vertice_id(b, u,literalsbefore_A)
-            B_v_lit = graph.get_Bbiclique_vertice_id(b, v,literalsbefore_B)
-            A_v_lit = graph.get_Abiclique_vertice_id(b, v,literalsbefore_A)
-            B_u_lit = graph.get_Bbiclique_vertice_id(b, u,literalsbefore_B)
-
-            cnf.append([-biclique_edge_lit, A_u_lit, B_u_lit, 0])
-            cnf.append([-biclique_edge_lit, A_u_lit, A_v_lit, 0])
-            cnf.append([-biclique_edge_lit, B_u_lit, B_v_lit, 0])
-            cnf.append([-biclique_edge_lit, B_v_lit, A_v_lit, 0])
-            cnf.append([-biclique_edge_lit, -A_u_lit, -B_u_lit,-A_v_lit, -B_v_lit, 0])
-
-
-
-    # if a vertice is in A_k then all other 
+    # If a vertex is in A part of a biclique then it has to have edges to all vertices in biclique's B part and none to A part:
     for u in graph.vertices:
-        for b in range(1,k+1):
-            A_u_lit = graph.get_Abiclique_vertice_id(b, u,literalsbefore_A)
+        for b in range(1, k+1):
+            A_u = graph.get_A_var(b, u, lits_before=lits_before_A)
             for v in graph.vertices:
                 if u != v:
-                    A_v_lit = graph.get_Abiclique_vertice_id(b, v,literalsbefore_A)
-                    B_v_lit = graph.get_Bbiclique_vertice_id(b, v,literalsbefore_B)
-                    b_edge = graph.get_biclique_edge_id(b, u, v,literalsbeforeedges)
-                    cnf.append([-A_u_lit, -A_v_lit, -b_edge, 0])
-                    cnf.append([-A_u_lit, -B_v_lit, b_edge, 0])
-
-    
-
+                    A_v = graph.get_A_var(b, v, lits_before=lits_before_A)
+                    B_v = graph.get_B_var(b, v, lits_before=lits_before_B)
+                    b_edge = graph.get_biclique_edge_var(b, u, v, lits_before=lits_before_edges)
+                    cnf.append([-A_u, -A_v, -b_edge, 0])
+                    cnf.append([-A_u, -B_v, b_edge, 0])
+    # If a vertex is in B part of a biclique then it has to have edges to all vertices in biclique's A part and none to B part:
+    for u in graph.vertices:
+        for b in range(1, k+1):
+            B_u = graph.get_B_var(b, u, lits_before=lits_before_B)
+            for v in graph.vertices:
+                if u != v:
+                    A_v = graph.get_A_var(b, v, lits_before=lits_before_A)
+                    B_v = graph.get_B_var(b, v, lits_before=lits_before_B)
+                    b_edge = graph.get_biclique_edge_var(b, u, v, lits_before=lits_before_edges)
+                    cnf.append([-B_u, -B_v, -b_edge, 0])
+                    cnf.append([-B_u, -A_v, b_edge, 0])
 
     return cnf, nr_vars
+
 
 def call_solver(cnf, nr_vars, output_file, solver="glucose-syrup", verb=0):
     # write the CNF formula to a DIMACS file
@@ -155,6 +143,25 @@ def call_solver(cnf, nr_vars, output_file, solver="glucose-syrup", verb=0):
             file.write(" ".join(map(str, clause)) + "\n")
 
     return subprocess.run(['./' + solver, '-model', '-verb=' + str(verb) , output_file], stdout=subprocess.PIPE)
+
+import re
+
+def parse_glucose_model(output_bytes):
+
+
+    text = output_bytes.decode()
+
+    if "UNSATISFIABLE" in text:
+        return None
+    if "SATISFIABLE" not in text:
+        return None
+
+    # All integers between lines starting with 'v'
+    literals = []
+    for line in text.splitlines():
+        if line.startswith("v"):
+            literals.extend([int(x) for x in line[1:].split() if x != '0'])
+    return literals
 if __name__ == "__main__":
 
     parser = ArgumentParser()
@@ -162,7 +169,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-i",
         "--input",
-        default="instances/instance3.in",
+        default="instances/instanceK32.in",
         type=str,
         help=(
             "The instance file."
@@ -187,18 +194,30 @@ if __name__ == "__main__":
     
     # encode the problem to create CNF formula
     k = 1
+    completetime = 0
+    kth_time_list = []
     while k <= maximum_iterations or maximum_iterations == 0:
     
         cnf, nr_vars = encode(graph,k)
         print("Running solver for", k, "bicliques...")
+
+        start_time = time.time()
         result = call_solver(cnf, nr_vars, args.output, "glucose-syrup", 1)
+        end_time = time.time()
+        kth_time = end_time - start_time
+        completetime += kth_time
+        kth_time_list.append(kth_time)
+    
         if result.returncode == 10:
             print("Bipartite dimension is", k)
+            print("Total time taken:", completetime, "s")
+            print(f"Time for last iteration ({k}): {kth_time} s")
+
+            model = parse_glucose_model(result.stdout)
             break
+        else:
+            print("No solution for", k, "bicliques.")
         k += 1
-
-
-    # call the SAT solver and get the result
-    #result = call_solver(cnf, nr_vars, args.output, args.solver, args.verb)
-
-    # interpret the result and print it in a human-readable format
+    
+    if k > maximum_iterations and maximum_iterations != 0:
+        print("Bipartite dimension is greater than", maximum_iterations)
